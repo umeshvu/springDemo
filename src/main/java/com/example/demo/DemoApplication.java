2f4e1006bad2d0562e432a561eed0d84f2ecd3af
umeshvu <94496768+umeshvu@users.noreply.github.com>
2025-11-18 08:10:24 +0530

    Implement hello endpoint in DemoApplication
    Added a REST endpoint that returns 'Hello world!'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		@GetMapping("/")
+  		public String hello(){ return "Hello world!"; }
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
